d50d6cd065715a168e2e9e61c1b077baeab1e595
md sanowar alam <atifreza72ar@gmail.com>
2022-06-27 23:45:37 +0530

    wjab

---
+++ index.js
@@ -0,0 +1,41 @@
+console.log("hello world");
+
+
+// var num=5;
+// let sonu=7+num++;
+// let lonu= 7+num;
+
+// console.log(num);
+// console.log(sonu);
+
+// console.log(12-"5");
+
+// let sonu="dabba";
+// let akshata="daabba";
+// console.log((sonu==akshata)?'its matched':"its not matched");
+
+
+// function add (a,b=5){
+//     return a+b;
+
+// }
+// var asum= add(10);
+// console.log(asum); 
+// const arr=["aam","anar","impli","kaitha"];
+// for(let i of arr){
+//     console.log(i);
+
+// }
+// const arr=[1,2,3,4,5,6,7];
+// let a=arr.slice(1,4);
+// console.log(a);
+// console.log(arr);
+// const fil=arr.map((i)=>{
+//     return i+3;
+// }).reduce((i,j)=>{
+//     return i+=j;
+// })
+// console.log(fil);
+// const s= "sonu is a good boy";
+// let m= new String(s);
+// console.log(m);
