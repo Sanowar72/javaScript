de4a69fb0f94a50a2a3f98b55c744c44e2d53f19
md sanowar alam <atifreza72ar@gmail.com>
2022-10-10 21:35:04 +0530

    commit

--- index.js
+++ index.js
@@ -144,7 +144,8 @@
     // const obj1= myobj;
     // // myobj.bi.name='hi sonu'
     // obj1.name='somthing';
-    // console.log(obj1);
+    // console.log(obj1,myobj);
+    // // console.log(myobj.bi);
 
     // by spread operator
 
@@ -171,8 +172,7 @@
     //     }
     //          mydata(){
     //             return`hi my name is ${this.myname} my age is ${this.myage} my branch is ${this.mybranch}`;
-    //             // console.log("hiii");
-    //         }
+    //          }
         
     // }
     // const mynum = new student('sonu',24,'ise');
@@ -189,7 +189,7 @@
     //     }
       
     //       present() {
-    //        return `hii may name is ${this.myname} i have ${this.mycar} whose top speed is ${this.topspeed};
+    //        return `hii may name is ${this.myname} i have ${this.mycar} whose top speed is ${this.topspeed}`;
     //     }
     // }
       
@@ -226,7 +226,7 @@
     //  factorial of a number
     // function fac(a){
     //     var value=1;
-    //     for(var i=a;i>1;i--){
+    //     for(var i=a;i>=1;i--){
             
     //         value=value*i;
     //     }
@@ -307,3 +307,138 @@
 //      }
 // }
 // console.log(add(5)(6)(8)(2)(3)(4)());
+
+
+// let s="hbweHMIXLAbnwn heo kwieb hvgwbd giwrd chfst";
+// let m = s.toLowerCase();
+// console.log(m);
+// let u = s.toUpperCase();
+// console.log(u);
+// let l= s.split(" ").length;
+// console.log(l);
+
+// let arr="hello hii are you";
+// let word = arr.split(" ");
+// let sw= arr.charAt(0).toUpperCase()+arr.slice(1);
+// console.log(sw);
+
+
+// const mySentence = "freeCodeCamp is an awesome resource";
+// const words = mySentence.split(" ");
+
+// for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+// }
+
+// let mn = words.join(" ");
+// console.log(mn);
+
+// const mySentence = "freeCodeCamp is an awesome resource";
+// const words = mySentence.split(" ");
+// // console.log(words);
+
+// const s = words.map((word) => { 
+//     return word[0].toUpperCase()+ word.substring(1) ; 
+// })
+// .join(" ")
+// ;
+
+// console.log(s)
+
+
+// let arr=[23,45,54,98,704,103,12,32,45];
+// let arr=[1,2,3,4,5]
+// arr.sort((a,b)=>b-a);
+// arr.push(10);
+// arr.pop();
+// arr.unshift(20);
+// arr.splice(5,1,105);
+// let s=arr.reduce((accu,elem)=>{
+//     return accu=accu*elem;
+// })
+// let s= arr.filter((elem)=>{
+//     return elem>50;
+// });
+
+// let s= arr.map((megha)=>{
+//     return megha=megha+2;
+// });
+//   arr.forEach((elem)=>{
+//      return elem=elem+2;
+//  });
+
+
+// console.log(arr);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// let total = sum(5,10);
+// console.log(total);
+
+// const sum = (a,b) =>{
+//    let s=  a+b;
+//    s;
+// }
+// let total = sum(5,10);
+// console.log(total);
+
+// function sum (a){
+//     return a;
+// }
+
+// const sum = a =>a;
+   
+
+//  let total = sum(5);
+// console.log(total);
+
+// using idex of
+// function getarr(arr){
+//     const myarr=[];
+//     for(let i of arr){
+//         if(myarr.indexOf(i)==-1){
+//             myarr.push(i);
+//         }
+//     }
+//     console.log(myarr);
+// }
+// getarr([1,2,3,2,4,5,3,6]);
+// console.log(sarr);
+
+// using setof
+
+// function marr(arr){
+//     const narr=[...new Set(arr)];
+//     console.log(narr);
+// }
+// marr([1,4,7,9,2,7,1,9,4,7,3,5]);
+
+
+// let arr=[3,5,8,1,9,3,6,8,45];
+// arr.reverse();
+// console.log(arr);
+
+
+// const a=[1,2,3,4,5,6,7,8,9,10];
+// const b=[1,5,9,3,4];
+
+// function getvalue (a,b){
+//     const noval=[];
+// for(let i=0;i<a.length;i++){
+   
+//     if(b.includes(a[i])){
+//         continue;
+//     }
+//     noval.push(a[i]);
+// }
+//   return noval;
+// }
+// console.log(getvalue(a,b));
+
+
+// const arr=[1,2,3,4,5,6];
+// const sum = arr.reduce((acc,ele)=> {
+//     return acc=acc+ele;
+// });
+// console.log(sum);
